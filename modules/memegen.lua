a533d0d6d8ab44bd1483e8d6a576e1ed653c4b36
Oskar <saturdaysheroes.business@gmail.com>
2020-01-04 22:28:00 +0100

    added generatememe method

--- modules/memegen.lua
+++ modules/memegen.lua
@@ -4,5 +4,24 @@
 
 --]]----------------------------------------------
 return function(client)
-    --
+    local httpr = require("coro-http")
+    local json = require("json")
+    local querystring = require("querystring")
+
+    function mbot.generatememe(message, templateID, username, password, textTop, textBot)
+        local headers = {
+            template_id = templateID,
+            username = username,
+            password = password,
+            text0 = textTop,
+            text1 = textBot
+        }
+        coroutine.wrap(function()
+            local url = "https://api.imgflip.com/caption_image?template_id="..templateID.."&username="..username.."&password="..password.."&text0="..textTop.."&text1="..textBot
+            local res, body = httpr.request("POST", url, headers)
+            local results = json.decode(body)
+
+            message:reply(results["data"]["url"])
+        end)()
+    end
 end
