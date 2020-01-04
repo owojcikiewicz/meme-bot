7eb054235759cf732a0df6d6289cff08a5ae8561
Oskar <saturdaysheroes.business@gmail.com>
2020-01-04 23:37:56 +0100

    commended out stuff

--- modules/commands.lua
+++ modules/commands.lua
@@ -11,9 +11,9 @@ return function(client)
 		local content = message.content:lower()
 		local author = message.author
 		local member = message.guild.members:get(message.author.id)
-		local args = content:split(" ")
+		--local args = content:split(" ")
 		local channel = message.channel
 
-        mbot.generatememe(message, "178591752", "SaturdaysH_MEMEBOT", "__memebot__", "Niedzialajacy bot :(", "Dzialajacy bot ktory tworzy memy i w ogole zajebioza lol")
+        mbot.generatememe(message, content, "SaturdaysH_MEMEBOT", "__memebot__", "Niedzialajacy bot :(", "Dzialajacy bot ktory tworzy memy i w ogole zajebioza lol")
     end)
 end
