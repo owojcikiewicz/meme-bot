3459699d37eb9981ea8e77c8749f158b77697cee
Oskar <saturdaysheroes.business@gmail.com>
2020-01-04 23:37:19 +0100

    added language categories: general, commands

--- modules/language.lua
+++ modules/language.lua
@@ -5,10 +5,26 @@
 --]]----------------------------------------------
 return function(client)
     languages["en"] = {
-        ["commandTest"] = "Yep, I'm here!"
+        ["general"] = {
+            ["memeSent"] = "Here you go, ",
+            ["memeFail"] = "Meme generation unsuccessful, please check the parameters and try again!",
+            ["memeGenerating"] = "Meme generation in progress, please wait..."
+        },
+        ["commands"] = {
+            ["commandFormats"] = "formats",
+            ["commandGenerate"] = "generate"
+        }
     }
 
     languages["pl"] = {
-        ["commandTest"] = "Wszystko działa z tej stronie!"
+        ["general"] = {
+            ["memeSent"] = "Proszę bardzo, ",
+            ["memeFail"] = "Tworzenie mema nie powiodło się, sprawdź parametry i spróbuj ponownie!",
+            ["memeGenerating"] = "Mem jest w trakcie tworzenia, chwila..."
+        },
+        ["commands"] = {
+            ["commandFormats"] = "szablony",
+            ["commandGenerate"] = "generuj"
+        }
     }
 end
